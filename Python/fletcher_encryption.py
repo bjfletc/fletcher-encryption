15cdac24988f8b1bd34808e8cbf8f0e6cd64d7c3
Brandon Fletcher <brandonjfletcher@gmail.com>
2021-08-12 17:46:24 -0500

    Set the configuration upon running the application.
    Added some commenting to the choose_fiel_btn as well as a TODO.

--- Python/fletcher_encryption.py
+++ Python/fletcher_encryption.py
@@ -7,12 +7,28 @@
 
 from tkinter import *
 
+# Setup and configure the app window.
 app = Tk()
 app.title("Fletcher Encryption")
 app.geometry("900x450")
+app.columnconfigure(0, minsize=300)
+app.columnconfigure(1, minsize=300)
+app.columnconfigure(2, minsize=300)
 
+# Going to allow the user to choose the file to encrypte/decrypte
+# with the application.
+# TODO: Implement choosing the file command.
 choose_file_btn = Button(app, text="Choose File")
-
 choose_file_btn.grid(row=0, column=0, rowspan=2)
+
+enter_key_lbl = Label(app, text="Enter your encryption key:")
+enter_key_lbl.grid(row=0, column=1)
+
+encryption_key = Entry(app)
+encryption_key.grid(row=1, column=1)
+
+fun_btn = Button(app, text="Enc/Dec")
+fun_btn.grid(row=0, column=2, rowspan=2)
+
 
 app.mainloop()
