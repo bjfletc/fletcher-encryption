d9edb7a2c95a9eb33f0a5c32460a6b3f7ebe0d00
Brandon Fletcher <brandonjfletcher@gmail.com>
2021-08-12 22:52:58 -0500

    Creating a module to house the app's operations.

---
+++ Python/app_operations.py
@@ -0,0 +1,14 @@
+#
+#  app_operations.py
+#
+#
+#  Created by Brandon Fletcher on 8/12/21.
+#
+import os   # needed to access openssl
+
+def encrypt():
+    print("encrypt")
+
+
+def decrypt():
+    print("decrypt")
