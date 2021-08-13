8dfcf3df2ced72f4b82fe9b9c209110ba70ec0ef
Brandon Fletcher <brandonjfletcher@gmail.com>
2021-08-12 23:24:31 -0500

    Created the main operations I will be using in the application.

--- Python/app_operations.py
+++ Python/app_operations.py
@@ -4,11 +4,30 @@
 #
 #  Created by Brandon Fletcher on 8/12/21.
 #
+
 import os   # needed to access openssl
+from tkinter import *   # for getting the file
 
-def encrypt():
+def encrypt(file_path, encryption_key):
+    openssl_cmd = "openssl enc -aes-256-cbc -in " + '"' + file_path + '" ' + \
+    "-out " + '"' + file_path + ".enc" + '" ' + "-pass pass:" + '"' + \
+    encryption_key + '"'
     print("encrypt")
+    os.system(openssl_cmd)
 
 
-def decrypt():
+def decrypt(file_path, decryption_key):
+    file = file_path[:file_path.find(".enc")]
+    openssl_cmd = "openssl enc -aes-256-cbc -in " + '"' + file_path + '" ' + \
+    "-out " + '"' + file + '" -d ' + "-pass pass:" + '"' + decryption_key + '"'
     print("decrypt")
+    os.system(openssl_cmd)
+
+# used to give option to choose file.
+def get_file_path():
+    global file_path
+    # Open and return file path
+    # file_path= filedialog.askopenfilename(title = "Select A File", filetypes \
+    # = (("mov files", "*.png"), ("mp4", "*.mp4"), ("wmv", "*.wmv"), ("avi", \
+    #"*.avi")))
+    file_path= filedialog.askopenfilename(title = "Select A File")
