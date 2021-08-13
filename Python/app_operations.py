10f2194df0f61fca695e69de1ee62f382507f975
Brandon Fletcher <brandonjfletcher@gmail.com>
2021-08-12 23:25:29 -0500

    Removed the components that were not operation based.

--- Python/app_operations.py
+++ Python/app_operations.py
@@ -6,7 +6,6 @@
 #
 
 import os   # needed to access openssl
-from tkinter import *   # for getting the file
 
 def encrypt(file_path, encryption_key):
     openssl_cmd = "openssl enc -aes-256-cbc -in " + '"' + file_path + '" ' + \
@@ -22,12 +21,3 @@ def decrypt(file_path, decryption_key):
     "-out " + '"' + file + '" -d ' + "-pass pass:" + '"' + decryption_key + '"'
     print("decrypt")
     os.system(openssl_cmd)
-
-# used to give option to choose file.
-def get_file_path():
-    global file_path
-    # Open and return file path
-    # file_path= filedialog.askopenfilename(title = "Select A File", filetypes \
-    # = (("mov files", "*.png"), ("mp4", "*.mp4"), ("wmv", "*.wmv"), ("avi", \
-    #"*.avi")))
-    file_path= filedialog.askopenfilename(title = "Select A File")
