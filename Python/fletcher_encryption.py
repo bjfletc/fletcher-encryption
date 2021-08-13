48bd1937d98ab466bd5900b392f6428d463633c9
Brandon Fletcher <brandonjfletcher@gmail.com>
2021-08-12 21:49:43 -0500

    Created a multi-line comment that explains the radio buttons. Alos, renamed the fun_btn to operation_btn as this made more sense.

--- Python/fletcher_encryption.py
+++ Python/fletcher_encryption.py
@@ -7,6 +7,20 @@
 
 from tkinter import *
 
+# May need to add to its own module later
+# Used to set the text of the button that performs the operation
+def fun_btn_status():
+    num = status.get()
+    if (num == 1):
+        operation_btn.configure(text="Encrypt")
+        print("This should happen")
+    elif (num == 2):
+        operation_btn.configure(text="Decrypt")
+        print("This didn't happen")
+    else:
+        operation_btn.configure(text="Encrypt")
+
+
 # Setup and configure the app window.
 app = Tk()
 app.title("Fletcher Encryption")
@@ -31,24 +45,31 @@ enter_key_lbl.grid(row=0, column=1, sticky=S)
 encryption_key = Entry(app, show="*", width=27)
 encryption_key.grid(row=1, column=1, sticky=N)
 
-# Button used to actually encrypt/decrypt a file.
-# TODO: Set the button text to change based off of radio button.
 status = IntVar()
 status.get()
-operation_btn_text = "Encrypt"
 
+"""
+ This is used to choose the operation the be performed on the file.
+ When we choose the encrypt radio button, it will change the operation
+ button to encrypt status. Vice versa with choosing decrypt.
+
+ Needed to seperate this into its own frame, so it could occupy the whole
+ row in the grid geometry.
+"""
 radio_btn_frame = Frame(app)
 radio_btn_frame.grid(row=0, column=2)
 
-encrypt = Radiobutton(radio_btn_frame, text="Encrypt", variable=status, value=1)
-decrypt = Radiobutton(radio_btn_frame, text="Decrypt", variable=status, value=2)
+encrypt = Radiobutton(radio_btn_frame, text="Encrypt", variable=status, value=1\
+, command=fun_btn_status)
+decrypt = Radiobutton(radio_btn_frame, text="Decrypt", variable=status, value=2\
+, command=fun_btn_status)
 
 encrypt.pack()
 decrypt.pack()
 
 
-fun_btn = Button(app, text=operation_btn_text)
-fun_btn.grid(row=1, column=2, sticky=N)
-
+# Button used to actually encrypt/decrypt a file.
+operation_btn = Button(app, text="Encrypt")
+operation_btn.grid(row=1, column=2, sticky=N)
 
 app.mainloop()
